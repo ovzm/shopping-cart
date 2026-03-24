e023dbed73881bf0ecad7e0881d9bf8accba43c4
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2026-03-24 16:05:19 -0300

    feat: integrate API and implement product list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header"
+import Products from "./components/Products/Products"
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <Header />
+      <Products />
     </>
   )
 }
